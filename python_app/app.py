07fc89ddd6d612813e5d9ffea8ad121f8c4824f7
lavaraja <lavatechno@gmail.com>
2016-06-19 10:13:51 +0530

    rev3

---
+++ python_app/app.py
@@ -0,0 +1,32 @@
+__author__ = 'lavaraja'
+import flask
+from flask import Flask,session, request, flash, url_for, redirect, render_template, abort ,g
+from flask_security import login_required
+from flask_login  import LoginManager
+import mysql.connector
+import hashlib, uuid
+from functools import wraps
+from flask import g, request, redirect, url_for
+from passlib.handlers.sha2_crypt import sha256_crypt
+import datetime
+import time
+
+
+app = Flask(__name__)
+app.secret_key="secretkey"
+
+app.secret_key = 'xxxxyyyyyzzzzz'
+
+@app.route('/',methods=['GET'])
+def home():
+    error='h'
+    return render_template('test.html')   # return a string
+
+@app.route('/fetch', methods=['GET', 'POST'])
+def fetch():
+    dest=request.form['query']
+    print dest
+    return render_template('test2.html')   # return a string
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run()
